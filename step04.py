98a1de3412c0c04e7e5f76675a1787625a21aca4
Yuki Yoshida <developeryoshida@gmail.com>
2022-11-01 01:08:27 +0900

    Update filter_log

--- step04.py
+++ step04.py
@@ -1,14 +1,15 @@
+from cmath import log
 import sys
 from datetime import datetime
 import re
 
-def to_datatime(input_datetime):
+def to_datatime(str_datetime):
     '''
     str型の日時情報をdatetime型に変換する関数
 
     Parameters
     ------------
-    input_date: str
+    str_date: str
         エポック秒またはYYYY型で日時を表す。
 
     Returns
@@ -17,34 +18,31 @@ def to_datatime(input_datetime):
         YYYY型の日時を表す。
     '''
 
-    if input_datetime.isdecimal():
+    if str_datetime.isdecimal():
     # epoch秒の場合
-        date = datetime.fromtimestamp(int(input_datetime))
+        date = datetime.fromtimestamp(int(str_datetime))
     else:
     # YYYY-MM-DDでの入力の時
-        date = datetime.strptime(input_datetime, "%Y-%m-%d %H:%M:%S")
+        date = datetime.strptime(str_datetime, "%Y-%m-%d %H:%M:%S")
 
     return date
 
 def read_file(filename):
     '''
     読み込んだファイルの内容を取得する関数
-
     Parameters
     ------------
-    file_lines: list
-        テキストファイルの1行を1つの要素として格納している
-
+    filename: str
+        読み込み対象のファイル名を指定する
     Returns
     ------------
     file_lines: list
         テキストファイルの1行を1つの要素として格納している
-
     '''
 
-    file_lines = list() 
+    file_lines = list()
     try:
-        with open(filename, 'r', encoding='utf-8') as f:
+        with open(filename, 'r', encoding = 'utf-8') as f:
             for line in f.readlines():
                 if bool(line.strip()):
                     file_lines.append(line.strip())
@@ -59,106 +57,68 @@ def read_file(filename):
 
     return file_lines
 
-def arranged_input_data(file_lines, file_texts):
+def filter_log(log_lines, start_datetime, end_datetime):
     '''
-    読み込んだファイルの情報を整形する関数
-
-    月を数字に揃える(例　Janを1にする)
-    年の情報がないので2022とする
-    ipアドレスの降順に出力するのでipアドレスをキーとして辞書型のデータを作成する
-
+    ファイルを日時情報で絞り込む関数
     Parameters
     ------------
-    months: list
-        初めの要素は空白で英語の短縮表記の月名が格納されている。インデックス番号と月名が一致する。
-    ip_list: list
-        ipアドレスのみのリスト
-    date_list: list
-        日時のみのリスト
-    others: list
-        テキストからipアドレスと日時を抜いたデータ。キーワード検索の検索対象になる
-    arrenged_texts: list
-        整形したテキストを再度合わせたデータ(ipアドレス、整形した日時、その他の順) 
-    log: dict
-        key: ip_list
-        value: arrenged_texts
-        ipアドレスをキーとした辞書型データ
-    sorted_log: list
-        logをkeyであるipアドレスの降順に並び替えたデータ
-
+    log_lines: list
+        ログのリストデータ
+    start_datetime: datetime
+        絞り込みの開始時刻
+    end_datetime: datetime
+        絞り込みの終了時刻
+    match_datetime: match
+        対象のログに指定のフォーマットの時刻情報が存在しているかを確認したマッチオブジェクト
+    log_datetime: datetime
+        対象のログの日時情報
 
     Returns
     ------------
-    sorted_log: list
-        logをkeyであるipアドレスの降順に並び替えたデータ
-    ip_list: list
-
+    filter_log_lines: list
+        開始時刻と終了時刻の範囲内にあるログのリスト
     '''
 
-    # 月を数字にそろえる
-    months = (' ','Jan', 'Feb', 'Mar', 'Apr',
-           'May', 'Jun', 'Jul', 'Aug',
-           'Sep', 'Oct',  'Nov', 'Dec' )
+    filter_log_lines = list()
+    year_info = str(datetime.today().year)
 
-    for i in range(len(file_lines)):
-        for j in range(1,13): #1~12のjが月と一致
-            if file_texts[i][0] == months[j]:
-                file_texts[i][0] = str(j) 
+    for line in log_lines:
+        match_datetime = re.match('... ([1-9]|[12][0-9]|3[01]) ([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]', line)
+        if match_datetime:
+            log_datetime=datetime.strptime(year_info+match_datetime.group(), '%Y%b %d %H:%M:%S')
+            # 日時情報が指定の範囲内のログに絞り込む
+            if start_datetime < log_datetime < end_datetime:
+                filter_log_lines.append(line)
 
-    # ファイルの日時を抽出して、YYYY型に整形する
-    date_list = list()
-    for i in file_texts: 
-        if len(i[0]) == 1 and len(i[1]) == 1: 
-            date_list.append("2022" + "-" + "0" + i[0] + "-" + "0" + i[1] + " " + i[2] )
-        elif len(i[0]) == 2 and len(i[1]) == 1: 
-            date_list.append("2022" + "-" + i[0] + "-" + "0" + i[1] + " " + i[2] )
-        elif len(i[0]) == 1 and len(i[1]) == 2:
-            date_list.append("2022" + "-" + "0" + i[0] + "-" + i[1] + " " + i[2] )    
-        else:
-            date_list.append("2022" + "-" + i[0] + "-" + i[1] + " " + i[2] )  
+    return filter_log_lines
 
-    # date_listをfile_textsに戻す
-    for i in range(len(file_texts)):
-        del file_texts[i][0:3]
-        file_texts[i].insert(0,date_list[i])
-    
-    # リストの1番目の要素（ipアドレス）を基準に並び替える
-    sorted_file_texts = sorted(file_texts, reverse = False, key = lambda x:x[1]) 
-
-    # ipアドレスのみ格納するリストの作成
-    ip_list = list()
-    for i in sorted_file_texts :
-        ip_list.append(i[3])
-
-    return sorted_file_texts, ip_list
-
-def matching_ip_list(sorted_file_texts,start_datetime,end_datetime):
-    '''
-    ipアドレス基準でソート済みのテキストから指定の日時に合致するipアドレスを抽出する関数
-
-    Parameters
-    ------------
-    matched_ip_list: list
-        開始時刻と終了時刻の範囲内にあるipアドレスを格納
-    date: str
-        日時を順番に格納
-    ip: str
-        ipアドレスを順番に格納
-
-    Returns
-    ------------
-    matched_ip_list: list
-        開始時刻と終了時刻の範囲内にあるipアドレスを格納
-    '''
-
-    matched_ip_list = list()
-    
-    for i in range(len(sorted_file_texts)):
-        date, ip = sorted_file_texts[i][0],sorted_file_texts[i][1]
-        if str(start_datetime) <= date and date <= str(end_datetime):
-            matched_ip_list.append(ip)
-
-    return matched_ip_list
+#def matching_ip_list(sorted_file_texts,start_datetime,end_datetime):
+#    '''
+#    ipアドレス基準でソート済みのテキストから指定の日時に合致するipアドレスを抽出する関数
+#
+#    Parameters
+#    ------------
+#    matched_ip_list: list
+#        開始時刻と終了時刻の範囲内にあるipアドレスを格納
+#    date: str
+#        日時を順番に格納
+#    ip: str
+#        ipアドレスを順番に格納
+#
+#    Returns
+#    ------------
+#    matched_ip_list: list
+#        開始時刻と終了時刻の範囲内にあるipアドレスを格納
+#    '''
+#
+#    matched_ip_list = list()
+#
+#    for i in range(len(sorted_file_texts)):
+#        date, ip = sorted_file_texts[i][0],sorted_file_texts[i][1]
+#        if str(start_datetime) <= date and date <= str(end_datetime):
+#            matched_ip_list.append(ip)
+#
+#    return matched_ip_list
 
 
 def main():
@@ -181,16 +141,16 @@ def main():
     '''
 
     args = sys.argv
-    
+
     if len(args) < 4:
         print("引数の数が足りません。", file = sys.stderr)
         sys.exit(1)
-    
+
     # ファイル名、開始日時、終了日時、キーワードを取得
-    file_name = args[1]
-    start_datetime = toDatatime(args[2])
-    end_datetime = toDatatime(args[3])
-    
+    logfile = args[1]
+    start_datetime = to_datatime(args[2])
+    end_datetime = to_datatime(args[3])
+
     #sstart_datetimeとend_datetimeが逆転してたらエラー
     if str(start_datetime) > str(end_datetime):
         print("開始時刻と終了時刻が逆です。", file = sys.stderr)
@@ -198,49 +158,49 @@ def main():
 
     if len(args) == 5:
         keywd = args[-1]
-    else:    
+    else:
         keywd = None
 
-    # 行ごとのファイル内容と、単語ごとのファイルの内容を取得
-    file_lines, file_texts = get_file(file_name)
+    # ファイルの内容を取得
+    file_lines = read_file(logfile)
 
-    # 整形したファイルの内容と、ipアドレスのみのリストを取得する
-    sorted_file_texts, ip_list = arranged_input_data(file_lines, file_texts)
+    # ファイルの内容を日時で絞り込む
+    filetered_log = filter_log(file_lines,start_datetime,end_datetime)
 
-    # 指定の日時が開始日時から終了日時までの範囲に該当するipアドレスのみを格納するリストを取得する
-    matched_ip_list = matching_ip_list(sorted_file_texts,start_datetime,end_datetime)
-
-    # ソート済みのipアドレス
-    for i in range(len(matched_ip_list)):
-        # 開始時刻と終了時刻に合致するipを順番に取得
-        ip = matched_ip_list[i]
-        # 日時とipアドレス以外をその他としてキーワード検索に使う
-        other = sorted_file_texts[i][2:]
-        
-        # 同じipアドレスがいつくあるかカウント
-        # 重複しないために同じipアドレスは一度しかカウントされない（一度カウントされたアドレスは２度目以降０としてカウントされる）
-        cnt = 0
-        for j in matched_ip_list:
-            if not ip in matched_ip_list[:i]:
-                if ip == j:
-                    cnt += 1
-
-        # ipアドレスをその数と一緒に出力する
-        # キーワードが指定されている場合はキーワードを含むもののみ出力する
-        if cnt >= 1:
-            # キーワード指定なしの場合
-            if keywd == None:
-                    print(ip,":", cnt)
-            # キーワード指定ありの場合    
-            else:
-                if keywd in other:
-                    print(ip,":", cnt)
-    
+#    # 指定の日時が開始日時から終了日時までの範囲に該当するipアドレスのみを格納するリストを取得する
+#    matched_ip_list = matching_ip_list(sorted_file_texts,start_datetime,end_datetime)
+#
+#    # ソート済みのipアドレス
+#    for i in range(len(matched_ip_list)):
+#        # 開始時刻と終了時刻に合致するipを順番に取得
+#        ip = matched_ip_list[i]
+#        # 日時とipアドレス以外をその他としてキーワード検索に使う
+#        other = sorted_file_texts[i][2:]
+#
+#        # 同じipアドレスがいつくあるかカウント
+#        # 重複しないために同じipアドレスは一度しかカウントされない（一度カウントされたアドレスは２度目以降０としてカウントされる）
+#        cnt = 0
+#        for j in matched_ip_list:
+#            if not ip in matched_ip_list[:i]:
+#                if ip == j:
+#                    cnt += 1
+#
+#        # ipアドレスをその数と一緒に出力する
+#        # キーワードが指定されている場合はキーワードを含むもののみ出力する
+#        if cnt >= 1:
+#            # キーワード指定なしの場合
+#            if keywd == None:
+#                    print(ip,":", cnt)
+#            # キーワード指定ありの場合
+#            else:
+#                if keywd in other:
+#                    print(ip,":", cnt)
+#
     sys.exit(0)
-                          
-   
+
+
 if __name__ == "__main__":
     main()
 
-       
+
 
